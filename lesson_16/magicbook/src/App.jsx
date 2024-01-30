880be00608057de3d955ea79d3e9003f9d9d7831
Yevhen Kim <evgeniikim@gmail.com>
2024-01-30 20:52:05 +0100

    + lesson_16

---
+++ lesson_16/magicbook/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import MagicBook from './components/MagicBook'; 
+import './components/MagicBook.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <MagicBook />
+    </div>
+  );
+}
+
+export default App;
